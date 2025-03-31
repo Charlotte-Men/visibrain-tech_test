6471276d776c0baa6dfceda7af29b3a3cd091a68
Charlotte Menard <charlotte.menard@live.fr>
2025-03-31 15:36:17 +0200

    Implement get_twitch_game_id service

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from backend.service.get_twitch_access_token import get_twitch_access_token
+from backend.service.get_twitch_game_id import get_twitch_game_id
  
 router = APIRouter()
 
 @router.get("/")
 async def root():
-    access_token = get_twitch_access_token()
+    game_id = get_twitch_game_id("civilization")
 
-    return {f"token: {access_token}"}
+    return {f"game id: {game_id}"}
--- backend/config.py
+++ backend/config.py
@@ -7,4 +7,5 @@ load_dotenv()
 TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
 TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
 
+TWITCH_GET_GAME_INFOS_URL="https://api.twitch.tv/helix/games"
 TWITCH_GET_TOKEN_URL="https://id.twitch.tv/oauth2/token"
