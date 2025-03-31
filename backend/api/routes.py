aad942d2992c1c0ef09a219f0855c955c8af01f1
Charlotte Menard <charlotte.menard@live.fr>
2025-03-31 15:48:01 +0200

    Implement get_videos_by_game service

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from backend.service.get_twitch_game_id import get_twitch_game_id
+from backend.service.get_twitch_videos_by_game import get_twitch_videos_by_game
  
 router = APIRouter()
 
 @router.get("/")
 async def root():
-    game_id = get_twitch_game_id("civilization")
+    videos = get_twitch_videos_by_game("civilization")
 
-    return {f"game id: {game_id}"}
+    return {f"Videos: {videos}"}
--- backend/config.py
+++ backend/config.py
@@ -9,3 +9,4 @@ TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
 
 TWITCH_GET_GAME_INFOS_URL="https://api.twitch.tv/helix/games"
 TWITCH_GET_TOKEN_URL="https://id.twitch.tv/oauth2/token"
+TWITCH_GET_VIDEOS_INFOS_URL="https://api.twitch.tv/helix/videos"
