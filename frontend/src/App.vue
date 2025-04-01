716cc9c6fc79dc4ae53e53334007c9cdf9fe86e9
Charlotte Menard <charlotte.menard@live.fr>
2025-04-01 13:45:32 +0200

    Create GameForm component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
+<script setup>
+  import GameForm from './components/GameForm.vue'
+</script>
+ 
 <template>
   <header>
     <h1>Twitch your game!</h1>
   </header>
 
   <main>
-    <div>Enter a game name to see last Twitch videos about it:</div>
+    <GameForm />
   </main>
 </template>
---
+++ frontend/src/components/GameForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form onsubmit="return false">
+    <p>Enter a game name to see last Twitch videos about it:</p>
+    <div>
+      <input 
+        v-model="gameName" 
+        @keyup.enter="searchVideos" 
+        placeholder="Game name"
+      />
+      <button @click="searchVideos">Search</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const gameName = ref("");
+
+    function searchVideos() {
+      if (gameName.value.trim()) {
+        console.log(gameName.value)
+      }
+    };
+
+    return { gameName, searchVideos };
+  },
+};
+</script>
