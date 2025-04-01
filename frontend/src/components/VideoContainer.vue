6199f917b2b0a40172dd34b2194fd333f841c512
Charlotte Menard <charlotte.menard@live.fr>
2025-04-01 16:13:37 +0200

    Add timer to refresh videos every 2min

--- frontend/src/components/VideoContainer.vue
+++ frontend/src/components/VideoContainer.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import GameForm from "./GameForm.vue";
 import VideoInfos from "./VideoInfos.vue";
 import { fetchVideosByGame } from "../services/api";
@@ -15,13 +15,31 @@ export default {
   components: { GameForm, VideoInfos },
   setup() {
     const videos = ref([]);
+    const currentGameName = ref("");
+    let intervalId = null;
 
     const fetchVideos = async (gameName) => {
+      // Save game name for video refresh
+      if (gameName) currentGameName.value = gameName;
+
       const result = await fetchVideosByGame(gameName);
       if (result.Videos) {
         videos.value = result.Videos;
       }
     };
+
+    // Refresh videos every 2min
+    onMounted(() => {
+      intervalId = setInterval(() => {
+        if (currentGameName.value) fetchVideos(currentGameName.value);
+      }, 120000);
+    });
+
+    // Clear timer on component unmount
+    onUnmounted(() => {
+      clearInterval(intervalId);
+    });
+
     return { videos, fetchVideos };
   },
 };
