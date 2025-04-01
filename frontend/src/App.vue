ed563c9e3fbe58359707220f569289add1128873
Charlotte Menard <charlotte.menard@live.fr>
2025-04-01 12:32:50 +0200

    Delete obsolete Vue demo content

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,9 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Twitch your game!</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <div>Enter a game name to see last Twitch videos about it:</div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
