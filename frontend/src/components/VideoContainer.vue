0a3a398aff60ff0f1207d7f22bf755a47bad0adb
Charlotte Menard <charlotte.menard@live.fr>
2025-04-01 15:46:49 +0200

    Create a VideoInfos component

--- frontend/src/components/VideoContainer.vue
+++ frontend/src/components/VideoContainer.vue
@@ -1,31 +1,27 @@
 <template>
   <GameForm @videos-found="fetchVideos" />
-  <div>
-    <div>{{ videos.length }}</div>
-    <div v-for="video in videos">
-      {{ video.title }} - {{ video.user_name }}
-    </div>
+  <div class="videos-list">
+    <VideoInfos v-for="video in videos" :key="video.id" :video="video" />
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
 import GameForm from "./GameForm.vue";
+import VideoInfos from "./VideoInfos.vue";
 import { fetchVideosByGame } from "../services/api";
 
 export default {
-  components: { GameForm },
+  components: { GameForm, VideoInfos },
   setup() {
     const videos = ref([]);
 
     const fetchVideos = async (gameName) => {
       const result = await fetchVideosByGame(gameName);
       if (result.Videos) {
-        console.log(result)
         videos.value = result.Videos;
       }
     };
-    console.log(videos.value)
     return { videos, fetchVideos };
   },
 };
---
+++ frontend/src/components/VideoInfos.vue
@@ -0,0 +1,19 @@
+<template>
+  <a :href="video.url" target="_blank">
+    <div class="video-wrapper">
+      <img :src="video.thumbnail_url" class="video-thumbnail"/>
+      <div class="video-text-wrapper">
+        <h3>{{ video.title }}</h3>
+        <p>By {{ video.user_name }}</p>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+  export default {
+    props: {
+      video: Object,
+    },
+  };
+</script>
